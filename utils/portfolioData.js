8c1a1f39a94d7138d196f74129f8dc38b81ec280
THIRULOGACHANDAR SRINIVASAN <THIRULOGACHANDAR>
2026-01-22 07:28:39 +0530

    skill.js

---
+++ utils/portfolioData.js
@@ -0,0 +1,27 @@
+export const frontendSkills = [
+  { id: 1, name: "HTML", image: "" },
+  { id: 2, name: "CSS", image: "" },
+  { id: 3, name: "Javascript", image: "" },
+  { id: 4, name: "React.Js", image: "" },
+  { id: 5, name: "Next.Js", image: "" },
+  { id: 6, name: "Tailwind CSS", image: "" },
+  { id: 7, name: "Material UI", image: "" },
+  { id: 8, name: "Redux Toolkit", image: "" },
+];
+
+
+export const tools =[
+  { id: 1, name: "VS Code", image: "" },
+  { id: 2, name: "Postman", image: "" },
+  { id: 3, name: "Git", image: "" },
+  { id: 4, name: "Mongo DB compass", image: "" },
+  { id: 5, name: "Mongo DB compass", image: "" },
+]
+
+export const Backend=[
+  { id: 1, name: "Node.js", image: "" },
+  { id: 2, name: "Express.Js", image: "" },
+  { id: 3, name: "Mongo DB", image: "" },
+  { id: 3, name: "Mongoose", image: "" },
+
+]
